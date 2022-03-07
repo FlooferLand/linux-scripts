b4e514cce93b7e4819d12061ac3b3fd8451d6f40
FlooferLand <76737186+FlooferLand@users.noreply.github.com>
2022-03-07 00:38:49 +0000

    Nvidia? more like, green sus (amogus)

---
+++ disable-nvidia-telemetry.sh
@@ -0,0 +1,34 @@
+
+#############################################################
+### Disables most/all proprietary NVIDIA driver telemetry ###
+#############################################################
+# Should be appended to /etc/hosts using the following command:
+# disable-nvidia-telemetry.sh >> etc/hosts
+#
+# The .sh extension is only there for syntax highlighting
+
+0.0.0.0 gfswl.geforce.com
+0.0.0.0 telemetry.nvidia.com
+0.0.0.0 gfe.nvidia.com
+0.0.0.0 telemetry-dce.gfe.nvidia.com
+0.0.0.0 events-dc1.gfe.nvidia.com
+0.0.0.0 activation.gfe.nvidia.com
+0.0.0.0 activation-dc1.gfe.nvidia.com
+0.0.0.0 telemetry.gfe.nvidia.com
+0.0.0.0 events.gfe.nvidia.com
+0.0.0.0 telemetry-dc1.gfe.nvidia.com
+0.0.0.0 telemetry.gfe.nvidia.com
+0.0.0.0 hqemgate00.nvidia.com
+0.0.0.0 hqemgate01.nvidia.com
+0.0.0.0 hqemgate02.nvidia.com
+0.0.0.0 hqemgate03.nvidia.com
+0.0.0.0 hqemgate04.nvidia.com
+0.0.0.0 hqemgate05.nvidia.com
+0.0.0.0 hqemgate06.nvidia.com
+0.0.0.0 hqemgate07.nvidia.com
+0.0.0.0 hqemgate08.nvidia.com
+0.0.0.0 hqemgate09.nvidia.com
+0.0.0.0 hqemgate10.nvidia.com
+0.0.0.0 hqemgate11.nvidia.com
+0.0.0.0 hqemgate12.nvidia.com
+0.0.0.0 nvidiahk.nvidia.com
