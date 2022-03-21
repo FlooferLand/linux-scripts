44f12d1193301ba1647baa2bff6525b571d0f17a
FlooferLand <76737186+FlooferLand@users.noreply.github.com>
2022-03-21 22:09:39 +0000

    Update bashrc.sh

--- bashrc.sh
+++ bashrc.sh
@@ -1,10 +1,13 @@
 
-# removes orphan configs (command is from https://www.youtube.com/watch?v=Ahp34-1PtBE)
-function technoblade() {
-	if [[ $1 == "more" ]]; then
-		# Lists descriptions and other info aswell
-		dpkg -l | awk '/^rc/'
-	else
-		dpkg -l | awk '/^rc/  {print($2)}'
-	fi
+function clean() {
+	# Remove orphan packages
+	sudo pacman -Rns $(pacman -Qtdq)
+	
+	# Remove home cache
+	rm -rf ~/.cache/*
+	
+	# Sync the file system
+	sync
 }
+
+
