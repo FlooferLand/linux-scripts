95ff518dc690dd918b6b19576b353422a4278357
FlooferLand <76737186+FlooferLand@users.noreply.github.com>
2022-03-25 01:36:40 +0000

    updated since i moved to Arch (btw)

--- setup.sh
+++ setup.sh
@@ -2,12 +2,11 @@
 
 ###################################################################
 # A script i made so i don't have to remember realy long commands #
-# Only tested on Debian 11.                                       #
 # Script must be ran as root!                                     #
 # Script is also sorta unfinished. I may add to it as i see fit.  #
 ###################################################################
 
 # Flatpak
-apt install flatpak
+pacman install flatpak
 #apt install gnome-software-plugin-flatpak  # For GNOME Software
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
